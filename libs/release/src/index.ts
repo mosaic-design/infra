80b40d1afd9eb8f1d0110c0f6537c681b4ee11a1
Safin Ruslan <r.safin89@gmail.com>
2023-01-31 16:39:48 +0300

    pipelines

--- libs/release/src/index.ts
+++ libs/release/src/index.ts
@@ -4,3 +4,4 @@ export * from './release/stage-release-commit';
 export * from './release/publish-release-ci';
 export * from './release/publish-release-from-dist';
 export * from './release/changelog';
+
